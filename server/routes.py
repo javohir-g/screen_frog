5421ae648d42160aeb9d61f20cca5ff7bc02ee94
javohir-g <javohir-g@github.com>
2025-10-30 21:30:16 +0500

    add google vision

--- server/routes.py
+++ server/routes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
-from ocr import extract_text
+from ocr_reader import extract_text
 from ai_handler import analyze_text
 from telegram_bot import send_to_telegram, send_photo, send_pre
 import traceback
@@ -29,7 +29,7 @@ async def process(file: UploadFile = File(...)):
             send_to_telegram(result)
         except Exception as e:
             print(f"⚠️ Ошибка отправки ответа ИИ в Telegram: {e}")
-        return {"status": "ok", "result": result}
+        return {"status": "ok", "ocr": text, "result": result}
     except Exception as e:
         error_msg = f"Internal error: {str(e)}"
         print(f"❌ Ошибка обработки: {error_msg}")
