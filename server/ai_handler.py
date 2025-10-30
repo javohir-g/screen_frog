0a2d6b3699593e967d179163fddeed26137fea3e
javohir-g <javohir-g@github.com>
2025-10-30 15:57:27 +0500

    added promt

--- server/ai_handler.py
+++ server/ai_handler.py
@@ -1,13 +1,16 @@
 import os
 import google.generativeai as genai
+from prompt_manager import load_prompt
 
 GEMINI_API_KEY = os.environ["GEMINI_API_KEY"]
 genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel('gemini-2.5-pro')  # Можно заменить модель на flash если нужно
 
 def analyze_text(text: str):
+    system_prompt = load_prompt()
+    prompt = f"{system_prompt}\n\n{text}"
     try:
-        response = model.generate_content(text)
+        response = model.generate_content(prompt)
         return response.text
     except Exception as e:
         return f"GEMINI SDK error: {e}"
